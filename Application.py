aea4880cdd8441de1b8f00178920210c137269e6
Purwansh Chaudhary <purwanshchaudhary@gmail.com>
2025-09-04 14:55:25 +0530

    Application.py

--- Application.py
+++ Application.py
@@ -1,13 +1,13 @@
 import streamlit as s
 import qrcode
 
-s.title("QR Code Generator ⚡⚡⚡")
+s.title("QR Code Generator")
 orcode = s.text_input("Enter the URL or Link : ")  
 
 
 app_name = s.text_input("Enter File Name : ( give your QR Code a name ) : ")
 
-if s.button('GENERATE'):
+if s.button('GENERATE ⚡'):
     makeqr = qrcode.make(orcode)
     makeqr.save(f"{app_name}.png")
 
@@ -17,6 +17,7 @@ if s.button('GENERATE'):
 
 
     
+
 
 
 
